4d245c199468eb01d95daf7c1f7a8a42c67917ad
keshav-star <61754543+keshav-star@users.noreply.github.com>
2023-09-02 16:07:45 +0530

    Update posts.controller.js

--- controller/posts.controller.js
+++ controller/posts.controller.js
@@ -3,7 +3,7 @@ const postsController = {
 
     getAll: async (req, res) => {
         try {
-            const [rows, fields] = await pool.query("select * from api_data")
+            const [rows, fields] = await pool.query("select * from API_Data")
             res.json({
                 data: rows
             })
@@ -14,7 +14,7 @@ const postsController = {
     getById: async (req, res) => {
         try {
             const { id } = req.params
-            const [rows, fields] = await pool.query("select * from api_data where T_ID = ?", [id])
+            const [rows, fields] = await pool.query("select * from API_Data where T_ID = ?", [id])
             res.json({
                 data: rows
             })
